b62aa7d0de91d513fc0708c0ea3a6756b1989752
BAIR <clubesdosul@gmail.com>
2026-07-30 17:16:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -143,10 +143,10 @@ if botao_gerar:
                     cor_texto = mapa_cores[cor_legenda]
 
                     try:
-                        font = ImageFont.truetype("fonte.ttf", 85)
+                        font = ImageFont.truetype("fonte.ttf", 60)
                     except:
                         try:
-                            font = ImageFont.truetype("DejaVuSans-Bold.ttf", 85)
+                            font = ImageFont.truetype("DejaVuSans-Bold.ttf", 60)
                         except:
                             font = ImageFont.load_default()
 
