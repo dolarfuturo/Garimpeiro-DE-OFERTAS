3746f421072d78d5268d1844b0f5bb1dd5624e08
BAIR <clubesdosul@gmail.com>
2026-07-31 14:19:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ if botao_gerar:
                 Retorne APENAS o texto puro, sem indicações de cena, sem aspas, sem asteriscos, sem hashtags e sem parênteses."""
                 
                 response = client.models.generate_content(
-                    model='gemini-2.0-flash',
+                    model='gemini-1.5-flash',
                     contents=prompt,
                 )
                 
